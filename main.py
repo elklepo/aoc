b7f2b9eece8a346838c6c19ffc670624ab047ba4
elklepo <klepackijacek94@gmail.com>
2022-12-12 19:07:40 +0100

    reorganize files

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import time
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        exit(f'usage: python3 {sys.argv[0]} <day_number>')
+    if len(sys.argv) < 3:
+        exit(f'usage: python3 {sys.argv[0]} <year> <day>')
 
-    day = sys.argv[1]
+    year, day = sys.argv[1], sys.argv[2]
+    day_dir_name = f'day{day.rjust(2, "0")}'
 
-    day_module = importlib.import_module(f'day{day}.xpl')
+    day_module = importlib.import_module(f'{year}.{day_dir_name}.xpl')
 
-    with open(f'./day{day}/input0', 'r') as f:
+    with open(f'{year}/{day_dir_name}/input0', 'r') as f:
         cnt0 = f.read().rstrip()
 
-    with open(f'./day{day}/input1', 'r') as f:
+    with open(f'{year}/{day_dir_name}/input1', 'r') as f:
         cnt1 = f.read().rstrip()
-
 
     def _measure_time(foo):
         start = time.time()
@@ -24,15 +24,15 @@ if __name__ == '__main__':
         print(f'took {time.time() - start:.2f}s')
 
     if hasattr(day_module, 'task1'):
-        print("### TASK 1 - TEST INPUT ###")
+        print(f'### {year} DAY {day} - TASK 1 - TEST INPUT ###')
         _measure_time(lambda: day_module.task1(cnt0))
 
-        print("### TASK 1 - CHALLENGE INPUT ###")
+        print(f'### {year} DAY {day} - TASK 1 - CHALLENGE INPUT ###')
         _measure_time(lambda: day_module.task1(cnt1))
 
     if hasattr(day_module, 'task2'):
-        print("### TASK 2 - TEST INPUT ###")
+        print(f'### {year} DAY {day} - TASK 2 - TEST INPUT ###')
         _measure_time(lambda: day_module.task2(cnt0))
 
-        print("### TASK 2 - CHALLENGE INPUT ###")
+        print(f'### {year} DAY {day} - TASK 2 - CHALLENGE INPUT ###')
         _measure_time(lambda: day_module.task2(cnt1))
