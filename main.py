50195e115c9d6c953cbf59a4c1634ff6e6af24fd
Jacek Klepacki <klepackijacek94@gmail.com>
2024-12-11 07:45:17 +0100

    improve the execution harness

--- main.py
+++ main.py
@@ -24,15 +24,19 @@ if __name__ == '__main__':
         print(f'took {time.time() - start:.2f}s')
 
     if hasattr(day_module, 'task1'):
-        print(f'### {year} DAY {day} - TASK 1 - TEST INPUT ###')
-        _measure_time(lambda: day_module.task1(cnt0))
+        if cnt0:
+            print(f'### {year} DAY {day} - TASK 1 - TEST INPUT ###')
+            _measure_time(lambda: day_module.task1(cnt0))
 
-        print(f'### {year} DAY {day} - TASK 1 - CHALLENGE INPUT ###')
-        _measure_time(lambda: day_module.task1(cnt1))
+        if cnt1:
+            print(f'### {year} DAY {day} - TASK 1 - CHALLENGE INPUT ###')
+            _measure_time(lambda: day_module.task1(cnt1))
 
     if hasattr(day_module, 'task2'):
-        print(f'### {year} DAY {day} - TASK 2 - TEST INPUT ###')
-        _measure_time(lambda: day_module.task2(cnt0))
+        if cnt0:
+            print(f'### {year} DAY {day} - TASK 2 - TEST INPUT ###')
+            _measure_time(lambda: day_module.task2(cnt0))
 
-        print(f'### {year} DAY {day} - TASK 2 - CHALLENGE INPUT ###')
-        _measure_time(lambda: day_module.task2(cnt1))
+        if cnt1:
+            print(f'### {year} DAY {day} - TASK 2 - CHALLENGE INPUT ###')
+            _measure_time(lambda: day_module.task2(cnt1))
